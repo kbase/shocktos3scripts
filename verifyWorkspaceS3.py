abf36256c99974906e5c39163f898199397de185
kkellerlbl <kkeller@lbl.gov>
2020-09-08 16:50:51 -0700

    Update verifyWorkspaceS3.py

--- verifyWorkspaceS3.py
+++ verifyWorkspaceS3.py
@@ -117,7 +117,7 @@ def main():
 	    s3stat = s3.head_object(Bucket=CONFIG_S3_BUCKET,Key=s3doc['chksum'])
 	    pprint (s3stat)
 	except botocore.exceptions.ClientError as e:
-	    if '404' in e.message:
+	    if '403' in e.message:
 	        count_bad_s3 += 1
 	        pprint(COLLECTION_SHOCK + ' node ' + node['node'] + ' is missing matching resource in S3 ' + CONFIG_S3_ENDPOINT)
 	    else:
