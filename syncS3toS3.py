c0ef7bcffc5a397ba8d76c0bc9b54fef7d367463
kkellerlbl <kkeller@lbl.gov>
2020-11-24 17:07:59 -0800

    Update syncS3toS3.py

--- syncS3toS3.py
+++ syncS3toS3.py
@@ -79,7 +79,7 @@ def getObjects(start):
   idQuery = {'_id': {'$gt': bson.ObjectId.from_datetime(start) } }
  
   for object in db[conf['main']['mongo_collection']].find(idQuery):
-    pprint(object)
+#    pprint(object)
     ids.append(object['key']) #.split(': u\'')[1].replace("'}\n",''))
     ct+=1
   return ids
