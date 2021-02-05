4f7f014db4fa30f4f5818a3b84d1d8b32cd4c362
kkellerlbl <kkeller@lbl.gov>
2021-02-05 14:34:25 -0800

    Update syncS3toS3.py

--- syncS3toS3.py
+++ syncS3toS3.py
@@ -102,7 +102,7 @@ def syncnode(id):
   if (conf['main']['mode'] == 'blobstore'):
     spath="%s/%s/%s/%s/%s/%s"%(conf['source']['endpoint'],conf['source']['bucket'],id[0:2],id[2:4],id[4:6],id)
     dpath="%s/%s/%s/%s/%s/%s"%(conf['destination']['endpoint'],conf['source']['bucket'],id[0:2],id[2:4],id[4:6],id)
-    s3dpath="%s/%s/%s/%s/%s"%(id[0:2],id[2:4],id[4:6],id)
+    s3dpath="%s/%s/%s/%s"%(id[0:2],id[2:4],id[4:6],id)
 
   print "looking for %s at destination %s" % (id,s3dpath)
   try:
