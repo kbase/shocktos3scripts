d108258013ee65f8ecb5874ac1fbd3c7098d4fc2
kkellerlbl <kkeller@lbl.gov>
2021-03-05 22:02:50 -0800

    Update syncS3toS3.py

--- syncS3toS3.py
+++ syncS3toS3.py
@@ -226,12 +226,12 @@ if __name__ == '__main__':
 
   if int(conf['source']['insecure']) == 1:
     sslVerifySource = False
-    import botocore.vendored.requests.packages.urllib3 as urllib3
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+#    import botocore.vendored.requests.packages.urllib3 as urllib3
+#    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
   if int(conf['destination']['insecure']) == 1:
     sslVerifyDest = False
-    import botocore.vendored.requests.packages.urllib3 as urllib3
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+#    import botocore.vendored.requests.packages.urllib3 as urllib3
+#    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
   readlog(conf['main']['logfile'],done)
   readlog(conf['main']['retryfile'],retry)
