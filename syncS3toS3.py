5f5df36b8fa78100a01deba2265f540789188ebf
kkellerlbl <kkeller@lbl.gov>
2021-03-09 16:43:36 -0800

    Update syncS3toS3.py

--- syncS3toS3.py
+++ syncS3toS3.py
@@ -177,7 +177,7 @@ def syncnode(id):
       pprint(mcCommand)
     # result = call(mcCommand)
 ## if mc succeeds, remove file from tmpdir
-    os.unlink (localfile)
+#    os.unlink (localfile)
 
     return 1
 
