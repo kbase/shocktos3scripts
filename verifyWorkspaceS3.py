936b6974e8da2585bd79f749862d3f1c88223786
kkellerlbl <kkeller@lbl.gov>
2021-03-12 17:27:17 -0800

    Update verifyWorkspaceS3.py

--- verifyWorkspaceS3.py
+++ verifyWorkspaceS3.py
@@ -200,7 +200,7 @@ def main():
 
 #    for node in db[COLLECTION_SOURCE].find(idQuery, batch_size=CONFIG_BATCH_SIZE, no_cursor_timeout=True):
     pool = Pool(processes=CONFIG_NTHREADS)
-    results=pool.imap_unordered(verifyObject, db[COLLECTION_SOURCE].find(idQuery, batch_size=CONFIG_BATCH_SIZE, no_cursor_timeout=True), 100)
+    results=pool.imap_unordered(verifyObject, db[COLLECTION_SOURCE].find(idQuery, batch_size=CONFIG_BATCH_SIZE, no_cursor_timeout=True), 1000)
 
 #    pprint(results)
 
