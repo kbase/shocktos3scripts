aa5bb28cf14f42b3bdbf086fd75342c335ee32a3
kkellerlbl <kkeller@lbl.gov>
2021-03-18 14:50:57 -0700

    Update verifyS3.py

--- verifyS3.py
+++ verifyS3.py
@@ -136,7 +136,7 @@ def verifyObject(obj):
 # use this instead to simulate a 404
 #	    s3stat = s3.head_object(Bucket=CONFIG_S3_BUCKET,Key=obj[CHKSUM])
             pprint (s3stat)
-            pprint (obj[MD5_KEY])
+            pprint (obj[CHKSUM_KEY])
         except botocore.exceptions.ClientError as e:
 # if 404 not found, just note the missing object and continue
             if '404' in str(e):
